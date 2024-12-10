5b9b39e2c4f7c000cb51df5e0097b1953bfb75a4
sachin98765 <119880539+sachin98765@users.noreply.github.com>
2024-12-11 00:12:20 +0530

    2d array easy level question

--- 12_2d_arrays/TwoDArray.java
+++ 12_2d_arrays/TwoDArray.java
@@ -11,6 +11,22 @@ public class TwoDArray {
 
         int numbers[][]=new int[rows][cols];
 
+        //input
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                numbers[i][j]=sc.nextInt();
+            }
+
+        }
+
+        //output
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                System.out.print(numbers[i][j]+" ");
+            }
+            System.out.println(" ");
+        }
+
 
     }
     
