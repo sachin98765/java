706d3d758ab13dc1b7f6c5014727d1a50254dbb3
sachin98765 <119880539+sachin98765@users.noreply.github.com>
2024-12-10 23:58:42 +0530

    2d arrays

---
+++ 12_2d_arrays/TwoDArray.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class TwoDArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter Row Size---");
+        int rows=sc.nextInt();
+        System.out.println("Enter Column Size--");
+        int cols=sc.nextInt();
+
+        int numbers[][]=new int[rows][cols];
+
+
+    }
+    
+}
