7a9ee28ff3b1b5469baaadfe46012e594da0da6d
sachin98765 <119880539+sachin98765@users.noreply.github.com>
2024-12-17 20:39:43 +0530

    selection sort

--- 16_sorting/BubbleSort.java
+++ 16_sorting/BubbleSort.java
@@ -1,5 +1,7 @@
+import java.util.*;
 
 public class BubbleSort {
+
     public static void printArray(int arr[]) {
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+ " ");
@@ -8,8 +10,11 @@ public class BubbleSort {
         
     }
 
+
     public static void main(String[] args) {
+
         int arr[] ={7,8,3,1,2};
+
         //bubble sort
         for(int i=0; i<arr.length-1;i++){
             for(int j=0; j<arr.length-i-1;j++){
