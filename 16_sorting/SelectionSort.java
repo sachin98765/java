e5ef660c6796e0a205e442533fbcc0dc2532cbef
sachin98765 <119880539+sachin98765@users.noreply.github.com>
2024-12-19 21:43:27 +0530

    Update SelectionSort.java

--- 16_sorting/SelectionSort.java
+++ 16_sorting/SelectionSort.java
@@ -6,9 +6,7 @@ public class SelectionSort {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
-    }
-
-    
+    }  
     public static void main(String[] args) {
 
         int arr[]={7,8,3,1,2};
