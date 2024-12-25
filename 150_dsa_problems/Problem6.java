8d4327713d1f4d2d13b4f353e6d8f7516a16dd2a
sachin98765 <119880539+sachin98765@users.noreply.github.com>
2024-12-25 19:20:41 +0530

    Update Problem6.java

--- 150_dsa_problems/Problem6.java
+++ 150_dsa_problems/Problem6.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Problem6{
     public static void main(String[] args) {
+        //String Input
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a string or number:");
         String input = sc.next();
