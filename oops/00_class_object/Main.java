78692be6b3ad44b09ac2fbe5f5ec9cd94d1145f7
sachin98765 <119880539+sachin98765@users.noreply.github.com>
2025-01-08 23:07:37 +0530

    class nd object

--- oops/00_class_object/Main.java
+++ oops/00_class_object/Main.java
@@ -14,18 +14,41 @@ class Pen{
 
 
 }
+
+
+class Student{
+    String name;
+    int age;
+    String address;
+    
+    public void printInfo(){
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        Pen pen1=new Pen();
-        pen1.color="Red";
-        pen1.type="Ball";
 
-        Pen pen2=new Pen();
-        pen2.color="Blue";
-        pen2.type="Gel";
+        //this is for pen class
+        // Pen pen1=new Pen();
+        // pen1.color="Red";
+        // pen1.type="Ball";
 
-        pen1.printColor();
-        pen1.printColor();
+        // Pen pen2=new Pen();
+        // pen2.color="Blue";
+        // pen2.type="Gel";
+
+        // pen1.printColor();
+        // pen2.printColor();
+
+
+        //this is for student class
+        Student student1=new Student();
+        student1.name="Rahul";
+        student1.age=20;
+
+        student1.printInfo();
     }
     
 }
