112a839c87de0e1a888e8982c3f51e71eed8cdab
sachin98765 <119880539+sachin98765@users.noreply.github.com>
2025-01-09 00:05:21 +0530

    polymorphism(overloading)
    this overloading is happing compile time in polymorphism

---
+++ oops/02_polymorphism/01_function_oveloading/Main.java
@@ -0,0 +1,37 @@
+
+
+
+class Student{
+    String name;
+    int age;
+    String address;
+    
+    public void printInfo(String name){
+        System.out.println(name);
+    }
+
+    public void printInfo(int age){
+        System.out.println(age);
+    }
+
+    public void printInfo(String name, int age){
+        System.out.println(name + " " + age);   
+    }
+
+
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student student1= new Student();
+        student1.name = "John";
+        student1.age = 20;
+
+        student1.printInfo(student1.name,student1.age);
+
+
+    }
+    
+}
